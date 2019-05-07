6c2dbaa15fb3ef1a46fb7cc8bd8f8c233a48d09b
renatolmorais <renatolmorais@gmail.com>
2019-05-07 13:17:17 -0300

    main.py: cv2, PIL.Image, tesseract

--- main.py
+++ main.py
@@ -2,7 +2,10 @@
 
 # import the necessary packages
 import argparse
-import os
+import sys,os
+from PIL import Image
+import pytesseract
+import cv2
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
@@ -17,8 +20,16 @@ ap.add_argument(
         "-l",
         "--lang",
         type=str,
-        default="eng",
+        default="por",
         help="language to be used"
+        )
+		
+ap.add_argument(
+        "-p",
+        "--preprocess",
+        type=str,
+        default="thresh",
+        help="type of processing to be done"
         )
 		
 ap.add_argument(
@@ -31,4 +42,42 @@ ap.add_argument(
 
 args = vars(ap.parse_args())
 
-print args
+image_file = args['image']
+lang = args['lang']
+output = args['out']
+preprocess = args['preprocess']
+
+# load the example image and convert it to grayscale
+image = cv2.imread( image_file )
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+ 
+# check to see if we should apply thresholding to preprocess the
+# image
+if preprocess == "thresh":
+	gray = cv2.threshold(
+						gray,
+						0,
+						255,
+						cv2.THRESH_BINARY | cv2.THRESH_OTSU
+						)[1]
+ 
+# make a check to see if median blurring should be done to remove
+# noise
+elif preprocess == "blur": gray = cv2.medianBlur(gray, 3)
+ 
+# write the grayscale image to disk as a temporary file so we can
+# apply OCR to it
+filename = os.path.join( 'testes', '{}-processed.png'.format( os.path.basename(image_file) ) )
+print filename
+cv2.imwrite(filename, gray)
+
+#sys.exit(0)
+
+#print image_file,lang,output
+
+try:
+	img = Image.open( filename )
+	readed = pytesseract.image_to_string( img )
+	with open( output, 'w' ) as fp: fp.write( readed.encode('utf-8') )
+except Exception as err:
+	print 'error!'
